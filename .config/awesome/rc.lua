1ff0da0e8fb0f27b274f1894b202396129c7f860
Sergei <sepolish@student.jyu.fi>
2019-10-28 19:33:16 +0200

    remove volumeicon

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -659,5 +659,4 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- clip manager
 awful.util.spawn("copyq")
 awful.util.spawn("nm-applet")
-awful.util.spawn("volumeicon")
 awful.util.spawn("blueman-applet")
