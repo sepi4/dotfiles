b802276b332dca6db22e2510235c61693a575194
Sergei <sepolish@student.jyu.fi>
2020-03-04 01:22:01 +0200

    comment battery widget

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -239,7 +239,7 @@ awful.screen.connect_for_each_screen(function(s)
             wibox.widget.systray(),
             -- mytextclock,
             myCalendar,
-            battery_widget,
+            -- battery_widget,
             s.mylayoutbox,
             munteksti,
         },
