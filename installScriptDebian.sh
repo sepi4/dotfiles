b93ed8e9d547fb5144c421bc35f936d5fa5dfeb1
Sergei Polishsuk <sepolish@student.jyu.fi>
2020-03-07 06:16:23 +0200

    functions installScriptDebian

--- installScriptDebian.sh
+++ installScriptDebian.sh
@@ -4,36 +4,37 @@
 # apt install sudo -yy
 # /usr/bin/usermod -aG sudo $user
 
-cd ~
-homeDir=/home/$USER
-[ ! -d "$homeDir" ] && echo "no user dir: " && exit 1
+checkUserExists() {
+    cd ~
+    homeDir=/home/$USER
+    [ ! -d "$homeDir" ] && echo "no user dir: " && exit 1
+}
+
+
+checkDotfilesNotExist() {
+    dir=$user/dotfiles
+    [ -d "$dir" ]  && echo "'~/dotfiles' dir already exists, delete it" && exit 1
+}
+
+virtualBox() {
+    echo "give virtualbox version:"
+    read virtualboxVersion
+
+    mkdir ~/Downloads
+    address=https://download.virtualbox.org/virtualbox/$virtualboxVersion/VBoxGuestAdditions_$virtualboxVersion.iso
+    cd ~/Downloads
+    wget $address
+    file=VBoxGuestAdditions_$virtualboxVersion.iso
+    [ ! -f "$file" ] && echo "no file: $file" && exit 1
+    cd ~
+}
 
 
 
-dir=$user/dotfiles
-[ -d "$dir" ]  && echo "'~/dotfiles' dir already exists, delete it" && exit 1
 
 
-
-echo "give virtualbox version:"
-read virtualboxVersion
-
-mkdir ~/Downloads
-address=https://download.virtualbox.org/virtualbox/$virtualboxVersion/VBoxGuestAdditions_$virtualboxVersion.iso
-cd ~/Downloads
-wget $address
-file=VBoxGuestAdditions_$virtualboxVersion.iso
-[ ! -f "$file" ] && echo "no file: $file" && exit 1
-cd ~
-
-
-
-echo "NOW LONG INSTALL"
-sleep 2
-
-
-
-echo '# deb http://deb.debian.org/debian/ buster main
+addNonFree() {
+    echo '# deb http://deb.debian.org/debian/ buster main
 
 deb http://deb.debian.org/debian/ buster main contrib non-free
 deb-src http://deb.debian.org/debian/ buster main contrib non-free
@@ -45,55 +46,74 @@ deb-src http://security.debian.org/debian-security buster/updates main contrib n
 deb http://deb.debian.org/debian/ buster-updates main contrib non-free
 deb-src http://deb.debian.org/debian/ buster-updates main contrib non-free
 ' | sudo tee /etc/apt/sources.list
+}
+
+installApps()
+{
+    sudo apt update && sudo apt upgrade -yy
+
+    sudo apt install \
+        xorg git ranger firefox-esr htop net-tools awesome neovim copyq vlc \
+        npm xterm zsh curl dwm suckless-tools chromium w3m telegram-desktop thunar \
+        libx11-dev libxinerama-dev build-essential dkms network-manager-gnome \
+        linux-headers-$(uname -r) -yy
+    
+}
+
+getDotfiles() {
+    git clone https://github.com/sepi4/dotfiles.git ~/dotfiles
+}
+
+
+copyFromDotfiles() {
+    mkdir -p ~/.config
+    cp -r ~/dotfiles/.config/awesome/ ~/.config
+
+    git clone https://github.com/streetturtle/awesome-wm-widgets.git \
+        ~/.config/awesome/awesome-wm-widgets
+
+    cp -r ~/dotfiles/.config/nvim/ ~/.config
 
 
 
-sudo apt update && sudo apt upgrade -yy
-
-sudo apt install \
-xorg git ranger firefox-esr htop net-tools awesome neovim copyq vlc \
-npm xterm zsh curl dwm suckless-tools chromium w3m telegram-desktop thunar \
-libx11-dev libxinerama-dev build-essential dkms network-manager-gnome \
-linux-headers-$(uname -r) -yy
+    cat ~/dotfiles/.bashrc > ~/.bashrc
 
 
 
-
-git clone https://github.com/sepi4/dotfiles.git ~/dotfiles
-
-mkdir -p ~/.config
-cp -r ~/dotfiles/.config/awesome/ ~/.config
-
-git clone https://github.com/streetturtle/awesome-wm-widgets.git \
-~/.config/awesome/awesome-wm-widgets
-
-cp -r ~/dotfiles/.config/nvim/ ~/.config
+    cp ~/dotfiles/.Xresources ~/.Xresources
+    cp ~/dotfiles/git-prompt.sh ~/git-prompt.sh
+}
 
 
+addXinitrc() {
+    echo '#!/bin/sh
 
-cat ~/dotfiles/.bashrc > ~/.bashrc
-
-
-
-cp ~/dotfiles/.Xresources ~/.Xresources
-cp ~/dotfiles/git-prompt.sh ~/git-prompt.sh
-
-
-
-echo '#!/bin/sh
-
-[ -f ~/.Xresources ] && xrdb -merge ~/.Xresources
-exec awesome
-' > ~/.xinitrc
+    [ -f ~/.Xresources ] && xrdb -merge ~/.Xresources
+    exec awesome
+    ' > ~/.xinitrc
+}
 
 
+virtualBoxAppInstall() {
+    sudo mkdir /mnt/cd
+    sudo mount -t iso9660 -o ro,loop ~/Downloads/VBoxGuestAdditions_$virtualboxVersion.iso /mnt/cd/
+    cp /mnt/cd/VBoxLinuxAdditions.run ~/Downloads/VBoxLinuxAdditions.run
+    sudo umount /mnt/cd
+    sudo ~/Downloads/./VBoxLinuxAdditions.run
+}
 
-sudo mkdir /mnt/cd
-sudo mount -t iso9660 -o ro,loop ~/Downloads/VBoxGuestAdditions_$virtualboxVersion.iso /mnt/cd/
-cp /mnt/cd/VBoxLinuxAdditions.run ~/Downloads/VBoxLinuxAdditions.run
-sudo umount /mnt/cd
-sudo ~/Downloads/./VBoxLinuxAdditions.run
 
+echo "NOW starts LONG INSTALL"
+sleep 3
 
+checkUserExists
+checkDotfilesNotExist
+virtualBox
+addNonFree
+installApps
+getDotfiles
+copyFromDotfiles
+addXinitrc
+virtualBoxAppInstall
 
 echo "FINISHED!!!! now reboot your system"
