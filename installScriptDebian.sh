2d70882db491398de1bfc6979cc866b5664ee74a
Sergei <sepolish@student.jyu.fi>
2020-03-12 01:49:49 +0200

    undate installScriptDebian.sh

--- installScriptDebian.sh
+++ installScriptDebian.sh
@@ -11,11 +11,6 @@ checkUserExists() {
 }
 
 
-checkDotfilesNotExist() {
-    dir=$user/dotfiles
-    [ -d "$dir" ]  && echo "'~/dotfiles' dir already exists, delete it" && exit 1
-}
-
 virtualBox() {
     echo "give virtualbox version:"
     read virtualboxVersion
@@ -28,9 +23,6 @@ virtualBox() {
     [ ! -f "$file" ] && echo "no file: $file" && exit 1
     cd ~
 }
-
-
-
 
 
 addNonFree() {
@@ -53,9 +45,9 @@ installApps()
     sudo apt update && sudo apt upgrade -yy
 
     sudo apt install \
+        git \
         linux-headers-$(uname -r) \
         xorg \
-        git \
         ranger \
         firefox-esr \
         htop \
@@ -83,52 +75,16 @@ installApps()
         scrot \
         sxiv \
         pulseaudio \
-        thunar -yy
+        pcmanfm -yy
     
 }
 
-installVsCode() {
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/ -yy
-    sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' -yy
-
-    sudo apt-get install apt-transport-https -yy
-    sudo apt-get update -yy
-    sudo apt-get install code -yy
-}
 
 
-getDotfiles() {
-    git clone https://github.com/sepi4/dotfiles.git ~/dotfiles
-}
 
-
-copyFromDotfiles() {
-    mkdir -p ~/.config
-    cp -r ~/dotfiles/.config/awesome/ ~/.config
-
+awesomeWidgets() {
     git clone https://github.com/streetturtle/awesome-wm-widgets.git \
         ~/.config/awesome/awesome-wm-widgets
-
-    cp -r ~/dotfiles/.config/nvim/ ~/.config
-
-
-
-    cat ~/dotfiles/.bashrc > ~/.bashrc
-
-
-
-    cp ~/dotfiles/.Xresources ~/.Xresources
-    cp ~/dotfiles/git-prompt.sh ~/git-prompt.sh
-}
-
-
-addXinitrc() {
-    echo '#!/bin/sh
-
-    [ -f ~/.Xresources ] && xrdb -merge ~/.Xresources
-    exec awesome
-    ' > ~/.xinitrc
 }
 
 
@@ -145,28 +101,30 @@ addDotfilesGit() {
     git clone --bare https://github.com/sepi4/dotfiles.git $HOME/.cfg
 
     mkdir -p .config-backup && \
-        config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+        /usr/bin/git --git-dir=/home/sepi4/.cfg/ --work-tree=/home/sepi4 \
+        checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
         xargs -I{} mv {} .config-backup/{}
 
-    config checkout
-    config config --local status.showUntrackedFiles no
+    /usr/bin/git --git-dir=/home/sepi4/.cfg/ --work-tree=/home/sepi4 \
+        checkout
+
+    /usr/bin/git --git-dir=/home/sepi4/.cfg/ --work-tree=/home/sepi4 \
+        config --local status.showUntrackedFiles no
 }
 
 
 
 echo "CHECKINGS"
 checkUserExists
-checkDotfilesNotExist
 virtualBox
+
 echo "NOW starts LONG INSTALL"
 sleep 2
+
 addNonFree
 installApps
-installVsCode
-getDotfiles
-copyFromDotfiles
-addXinitrc
-virtualBoxAppInstall
 addDotfilesGit
+awesomeWidgets
+virtualBoxAppInstall
 
 echo "FINISHED!!!! now reboot your system"
