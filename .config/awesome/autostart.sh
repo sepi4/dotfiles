e8397e9c0eae3add1c0e263fdeb9416ef2d339d1
Sergei Polishsuk <polser1@gmail.com>
2026-01-17 10:44:35 +0200

    voidlinux changes

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -10,12 +10,11 @@ function run {
 run nm-applet # networkmanager, network-manager-applet (wifi)
 run copyq # copyq
 run blueman-applet # blueman (bluetooth)
-run pasystray # pasystray (volume) 
-# -- volumeicon (would be better, but not working)
+run volumeicon # for voidlinux
+# run pasystray # for archlinux (volumeicon not working)
 
 setxkbmap -option grp:win_space_toggle us,fi
 
 # -- also:
-# -- - noto-fonts (nice fonts using in awesome wm)
 # -- - lxappearance (to change themes of thunar, pcmanfm...)
 # -- - scrot, xclip (screenshots, nvim global clipboard)
