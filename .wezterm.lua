413fb0d430c2637430fae27260771510317d9ecd
Sergei Polishsuk <polser1@gmail.com>
2026-03-08 07:09:45 +0200

    add ghostty, and some updates

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,7 +8,7 @@ local config = wezterm.config_builder()
 config.font_size = 13
 config.harfbuzz_features = { "calt = 0", "clig = 0", "liga = 0" } -- Remove annoying special characters in coding.
 
-config.enable_tab_bar = true
+-- config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.warn_about_missing_glyphs = false
 
@@ -16,12 +16,19 @@ config.window_frame = {
 	border_left_width = "0.1cell",
 	border_right_width = "0.1cell",
 	border_bottom_height = "0.05cell",
-	-- border_top_height = "0.05cell",
+	border_top_height = "0.05cell",
 	border_left_color = "gray",
 	border_right_color = "gray",
 	border_bottom_color = "gray",
-	-- border_top_color = "gray",
+	border_top_color = "gray",
 }
+
+-- config.window_decorations = "NONE"  -- remove the ugly title bar
+config.enable_tab_bar = false -- if you don't need tabs
+config.window_padding = { left = 8, right = 8, top = 8, bottom = 8 }
+config.front_end = "WebGpu"   -- better rendering on some setups
+
+config.enable_scroll_bar = true
 
 -- config.window_padding = { -- remove pannings around
 -- 	left = 2,
