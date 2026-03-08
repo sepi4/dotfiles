ab6b8d7ccedfefde76e6b63dc64d56179ccb8ee9
Sergei Polishsuk <polser1@gmail.com>
2026-03-08 07:21:36 +0200

    Add a smart paste script for voxtype

---
+++ .config/voxtype/smart-paste.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Smart paste: Ctrl+Shift+V for terminals, Ctrl+V for GUI apps
+window_class=$(kdotool getactivewindow getwindowclassname 2>/dev/null)
+
+case "$window_class" in
+  *terminal*|*konsole*|*wezterm*|*alacritty*|*kitty*|*foot*|*gnome-terminal*|*xterm*)
+    ydotool key ctrl+shift+v
+    ;;
+  *)
+    ydotool key ctrl+v
+    ;;
+esac
