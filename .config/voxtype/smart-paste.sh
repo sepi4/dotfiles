8c91dbeb76be7ebab31816a1b1cd9b4bc33e887f
Sergei Polishsuk <polser1@gmail.com>
2026-07-03 16:54:19 +0300

    voxtype fix to smart-paste

--- .config/voxtype/smart-paste.sh
+++ .config/voxtype/smart-paste.sh
@@ -4,9 +4,11 @@ window_class=$(kdotool getactivewindow getwindowclassname 2>/dev/null)
 
 case "$window_class" in
   *terminal*|*konsole*|*wezterm*|*alacritty*|*kitty*|*ghostty*|*foot*|*gnome-terminal*|*xterm*)
-    ydotool key ctrl+shift+v
+    # Linux input keycodes: 29=Ctrl, 42=Shift, 47=V. :1 is down, :0 is up.
+    ydotool key 29:1 42:1 47:1 47:0 42:0 29:0
     ;;
   *)
-    ydotool key ctrl+v
+    # Linux input keycodes: 29=Ctrl, 47=V. :1 is down, :0 is up.
+    ydotool key 29:1 47:1 47:0 29:0
     ;;
 esac
