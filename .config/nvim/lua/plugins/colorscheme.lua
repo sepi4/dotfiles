ec177ff35aa1eb5602367a9a2206f0aed8ebf8e4
Sergei Polishsuk <polser1@gmail.com>
2026-08-04 04:32:47 +0300

    feat(ui): add more colorscheme options

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,21 @@
+-- Third-party colorschemes. Set a value to false to disable one.
+-- `lazy = false` keeps every variant available to :colorscheme completion.
+
+-- Neovim's built-in schemes are always available and are not managed here.
+
+-- Change this to select the colorscheme loaded at startup.
+local active_theme = "vscode"
+
 return {
   {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = active_theme,
+    },
+  },
+  {
     "Mofiqul/vscode.nvim",
+    enabled = true,
     lazy = false,
     priority = 1000,
     opts = {
@@ -10,9 +25,44 @@ return {
     },
   },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "vscode",
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    enabled = true,
+    lazy = false,
   },
+  {
+    "ellisonleao/gruvbox.nvim",
+    enabled = true,
+    lazy = false,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    enabled = true,
+    lazy = false,
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    enabled = true,
+    lazy = false,
+  },
+  {
+    "sainnhe/everforest",
+    enabled = true,
+    lazy = false,
+  },
+  {
+    "navarasu/onedark.nvim",
+    enabled = true,
+    lazy = false,
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    enabled = true,
+    lazy = false,
+  },
+  {
+    "xiantang/darcula-dark.nvim",
+    enabled = true,
+    lazy = false,
+  }
 }
